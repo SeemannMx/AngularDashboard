07b9eff7d44b74b2b06c48b342c11da931f97b4c
Tino Kallinich <tkallinich@inovex.de>
2018-07-18 13:11:29 +0200

    basic html css setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
-import { initClass } from "./runner/initClass";
-import {log} from "util";
-import {ChildProcessService} from "ngx-childprocess";
+import { Component  } from '@angular/core';
+import { log        } from "util";
 
 @Component({
   selector: 'app-root',
@@ -14,34 +12,11 @@ export class AppComponent {
   // declaration of variables
   title = 'Dashboard';
 
-  // declaration of objects
-  public xInitial:initClass;
-
   ngOnInit(){
-    this.callShellScript();
   }
 
   constructor() {
     log("app compo constructor");
-
-
-  }
-
-  callShellScript(){
-
-    log("initClass start this.callShellScript");
-    let require:any;
-
-    // run java jar to build database
-    let shell = require('shelljs');
-    shell.echo("jumpstart java dashboard to get data from https://ec.europa.eu");
-    shell.exec('echo');
-    shell.exec('java -jar /Users/tkallinich/DashboardProject/dashboard.jar');
-    shell.exec('echo');
-    shell.exec('echo  -e "\\033[0;32;5mfinished with Jumpstart\\033[0m"');
-
-    log("initClass end this.callShellScript");
-
   }
 
 }
